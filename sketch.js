0a73ee0c9ee0ef23ab6bdd1a1fe338521eef17f9
palat029 <TatyanaPalacios@tatyanas-mbp.wireless.newschool.edu>
2015-10-09 16:34:29 -0400

    UFO

--- sketch.js
+++ sketch.js
@@ -1,16 +1,40 @@
+background(50, 60, 80);
+
 function setup() {
 	// create a place to draw
 	createCanvas(640, 360);
 }
 
 function draw() {
-	// clear the background
-	background(50, 50, 50);
 
-	// set the drawing style
-	fill(100, 100, 100);
-	stroke(255, 255, 255);
+	// draw ground
 
-	// draw an ellipse
-	ellipse(320, 180, 100, 100);
+	if (mouseIsPressed) {
+		background(random(300), random(300), random(300));
+		fill(70, 90, 50);
+		noStroke();
+		rect(0, 260, 640, 90);
+
+		fill(0, 90, 0);
+		noStroke();
+		rect(0, 290, 640, 90);
+
+	} else {
+		background(50, 60, 80);
+		fill(70, 90, 50);
+		noStroke();
+		rect(0, 260, 640, 90);
+
+		fill(0, 90, 0);
+		noStroke();
+		rect(0, 290, 640, 90);
+
+
+	}
+	// draw UFO
+	fill(0, 0, 40);
+	ellipse(mouseX, mouseY, 60, 20);
+	fill(0, 0, 40);
+	ellipse(mouseX, mouseY, 40 - 20, 15);
+
 }
